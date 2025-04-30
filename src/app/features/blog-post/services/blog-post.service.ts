acc7091eceab56edd900fb87a8307c9cf6a6b378
vishal <vishalc@unifycloud.com>
2025-04-30 11:27:31 +0530

    fish upto section 8 blog post donw

--- src/app/features/blog-post/services/blog-post.service.ts
+++ src/app/features/blog-post/services/blog-post.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { addBlogPost } from '../models/add-blog-post.model';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blog-post-model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogPostService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  createBlogPost(data: addBlogPost): Observable<BlogPost>{
+return this.http.post<BlogPost>(`${environment.apibaseUrl}/api/blogposts`,data);
+  }
+
+  getAllBlogPosts(): Observable<BlogPost[]>{
+  return this.http.get<BlogPost[]>(`${environment.apibaseUrl}/api/blogposts`);
+  }
 }
