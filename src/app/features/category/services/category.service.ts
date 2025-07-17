d7f341ea31126783519fbc6da952a7613c6cf490
vishal <vishalc@unifycloud.com>
2025-07-17 23:03:01 +0530

    complete the UI up to date features

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -17,7 +17,7 @@ export class CategoryService {
   addCategory(model:AddCategoryRequest):Observable<void>{
     return this.http.post<void>(`${environment.apibaseUrl}/api/categories?addAuth=true`,model);
   }
-  getAllCategories(query?:string,sortBy?:string,sortDirection? :string):Observable<Category[]>{
+  getAllCategories(query?:string,sortBy?:string,sortDirection? :string,pageNumber?: number, pageSize?: number):Observable<Category[]>{
     let params= new HttpParams();
 
     if(query){
@@ -29,6 +29,13 @@ export class CategoryService {
     if(sortDirection){
       params=params.set('sortDirection',sortDirection);
     }
+    if (pageNumber) {
+      params = params.set('pageNumber', pageNumber)
+    }
+
+    if (pageSize) {
+      params = params.set('pageSize', pageSize)
+    }
   return this.http.get<Category[]>(`${environment.apibaseUrl}/api/categories`,{
     params:params
   });
@@ -36,6 +43,9 @@ export class CategoryService {
 
   getCategoryById(id: string): Observable<Category>{
   return this.http.get<Category>(`${environment.apibaseUrl}/api/categories/${id}`);
+  }
+  getCategoryCount(): Observable<number>{
+  return this.http.get<number>(`${environment.apibaseUrl}/api/categories/count`);
   }
   updateCategory(id:string,updateCategoryRequest:UpdateCategoryRequest):Observable<Category>
   {
